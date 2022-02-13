0a27b036067ecd18ffd12cc7179eea4e539504b6
Lohana Torres <lt.leopoldo@gmail.com>
2022-02-13 19:02:45 -0300

    feat: store transactions with mirage api

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Mercado',
-          amount: 400,
-          type: 'withdraw',
-          category: 'Food',
-          createdAt: new Date()
-        },
-        {
-          id: 2,
-          title: 'Salário',
-          amount: 3000.00,
-          type: 'deposit',
-          category: 'Job',
-          createdAt: new Date()
-        },
-        {
-          id: 3,
-          title: 'Curso Ignite',
-          amount: 814.93,
-          type: 'withdraw',
-          category: 'Courses',
-          createdAt: new Date()
-        },
-        {
-          id: 4,
-          title: 'Ração',
-          amount: 57.98,
-          type: 'withdraw',
-          category: 'Pet',
-          createdAt: new Date()
-        }
-      ]
-    })
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data);
+    });
   }
 })
 
