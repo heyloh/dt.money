b3e33e5b6a6649cc44f78b3e2f15c829ae986950
Lohana Torres <lt.leopoldo@gmail.com>
2022-02-13 19:17:14 -0300

    feat: use Intl for currency and date format

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -9,7 +9,7 @@ interface Transaction {
   amount: number;
   type: string;
   category: string;
-  createdAt: Date;
+  createdAt: string;
 }
 
 export function TransactionsTable() {
@@ -17,7 +17,7 @@ export function TransactionsTable() {
   useEffect(() => {
     api.get('transactions')
       .then((response: AxiosRequestConfig) => {
-        setTransactions(response.data);
+        setTransactions(response.data.transactions);
       });
   }, []);
 
@@ -36,9 +36,18 @@ export function TransactionsTable() {
           {transactions.map(transaction => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>R$ {transaction.amount}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
+              </td>
               <td>{transaction.category}</td>
-              <td>{transaction.createdAt.toLocaleString("pt-BR")}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
