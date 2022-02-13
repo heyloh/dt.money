90d3d20840caf2a16b19d6c886091d9e34be51eb
Lohana Torres <lt.leopoldo@gmail.com>
2022-02-13 19:16:50 -0300

    feat: fill schema with initial data

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance - Website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 5000,
+          createdAt: new Date('2022-02-05 10:07:00')
+        },
+        {
+          id: 2,
+          title: 'Contas',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1000,
+          createdAt: new Date('2022-02-11 14:26:00')
+        },
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
