4c19174e6276ddac31b61cdf612336fb3c484542
Lohana Torres <lt.leopoldo@gmail.com>
2022-02-13 12:46:43 -0300

    feat: add component: TransactionsTable

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">+ R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/01/2022</td>
+          </tr>
+          <tr>
+            <td>Contas</td>
+            <td className="withdraw">- R$1.100</td>
+            <td>Casa</td>
+            <td>17/01/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
